056edc4c789d2afafd095fced524002f2ea44795
MiguelRodrii <34117518+MiguelRodrii@users.noreply.github.com>
2021-01-25 18:41:25 -0500

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -12,20 +12,9 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <insertProductos />
-    </ApolloProvider>
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
